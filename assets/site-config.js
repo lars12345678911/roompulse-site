390f9a9f335ea2b096fc6981e41e461098d277d0
lars12345678911 <Lar.Ask@icloud.com>
2026-08-29 12:27:05 +0200

    Add assets/site-config.js

---
+++ assets/site-config.js
@@ -0,0 +1,14 @@
+/*
+  EDIT THIS FILE BEFORE YOU PUBLISH.
+  You only need to change the values below.
+*/
+window.ROOMPULSE_SITE = {
+  developerName: "Lars Ask",
+  supportEmail: "lars.ask@outlook.com",
+  appStoreUrl: "",
+  effectiveDate: {
+    nb: "29. august 2026",
+    en: "29 August 2026",
+    es: "29 de agosto de 2026"
+  }
+};
